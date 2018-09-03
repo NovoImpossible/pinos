1ca69cae7474cf430810aba61d231aad3ef1d2e8
Maksim Pinigin <pinigin@nvie.ru>
2018-09-03 19:56:48 +0700

    Pinos 0 03.09.18

--- core.py
+++ core.py
@@ -96,7 +96,7 @@ while True:
                 print('88"""  88 88 Y88 Yb   dP o.`Y8b     Yb   dP')
                 print('88     88 88  Y8  YbodP  8bodP      YbodP')
                 print('')
-                print("Pinos 0 Pre-alpha master")
+                print("Pinos 0 Pre-alpha 03.09.18")
                 print("Copyright (c) 2016-2018, Maksim Pinigin <maksim@pinig.in>")
                 print('')
             elif command[0] == "array":
--- pinos.cmd
+++ pinos.cmd
@@ -1,3 +1,3 @@
 @ECHO OFF
-echo Pinos 0 (c) 2016-2018, Maksim Pinigin. Pre-alpha master
+echo Pinos 0 (c) 2016-2018, Maksim Pinigin. Pre-alpha 03.09.18
 python C:\pinos\core.py
